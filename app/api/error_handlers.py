94abcd94541e799865dc4252c6d9b467d56eb47c
Nikita Churikov <nikita@chur.ru>
2021-10-21 19:25:37 +0300

    done presentation

--- app/api/error_handlers.py
+++ app/api/error_handlers.py
@@ -14,4 +14,4 @@ def handle_app_error(e: BaseAppError):
 def handle_exception(e: Exception):
     logger.exception(f"Unexpected error occurred '{e}'")
     unexpected_error = UnexpectedError()
-    return jsonify(unexpected_error.json), unexpected_error.json
+    return jsonify(unexpected_error.json), unexpected_error.error_code
